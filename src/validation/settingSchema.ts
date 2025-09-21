637765a9014b1a053c3637635bc5c646d7d782cb
Alexander Samaniego <alexanderwspenafiel@gmail.com>
2025-09-21 10:37:41 -0500

    adding the createdBy and updatedBy fields to settings

--- src/validation/settingSchema.ts
+++ src/validation/settingSchema.ts
@@ -5,6 +5,8 @@ export const SettingSchema = z.object({
     type: z.string().trim().min(1, 'Type is required').max(50, 'Type is too long'),
     config: z.string().trim().min(1, 'Config is required').max(50, 'Config is too long'),
     status: z.enum(['Active', 'Inactive']).optional().default('Active'),
+    createdBy: z.string().trim().min(1, 'Created by is required').max(50, 'Created by is too long'),
+    updatedBy: z.string().trim().min(1).max(50, 'Updated by is too long').optional(),
 });
 
 export const SettingPartialSchema = SettingSchema.partial();
